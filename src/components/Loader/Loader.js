4377f5ffffce6ebc4b5e3bb8ea604e123a95b143
dav55566666 <hovsepyandavid0@gmail.com>
2024-04-28 04:37:18 +0400

    a

--- src/components/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Loader() {
-    return (
-        <div className="load-screen" id="load-screen"><span /><span /><span /></div>
-    )
-}
-
-export default Loader
